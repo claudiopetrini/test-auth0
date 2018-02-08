04d9d0718862b4d8094b35a9a44f0f5f887958fd
Claudio Petrini <claudio.petrini@stamplay.com>
2018-02-08 17:35:21 +0100

    Create express-with-mlab-2.js

---
+++ express-with-mlab-2.js
@@ -0,0 +1,34 @@
+'use latest';
+import bodyParser from 'body-parser';
+import express from 'express';
+import Webtask from 'webtask-tools';
+import { MongoClient } from 'mongodb';
+const collection = 'my-collection';
+const server = express();
+server.use(bodyParser.json());
+server.get('/:id', (req, res, next) => {
+  const { MONGO_URL } = req.webtaskContext.data;
+  const { id } = req.params;
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) return next(err);
+    db.collection(collection).findOne({ id }, (err, result) => {
+      db.close();
+      if (err) return next(err);
+      res.status(200).send(result);
+    });
+  });
+});
+server.post('/', (req, res, next) => {
+  const { MONGO_URL } = req.webtaskContext.data;
+  // Do data sanitation here.
+  const model = req.body;
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) return next(err);
+    db.collection(collection).insertOne(model, (err, result) => {
+      db.close();
+      if (err) return next(err);
+      res.status(201).send(result);
+    });
+  });
+});
+module.exports = Webtask.fromExpress(server);
